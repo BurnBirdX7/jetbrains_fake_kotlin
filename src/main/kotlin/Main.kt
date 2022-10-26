16acc39a1b8e7eed8f4f2299430cada8ea02ce4e
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2022-10-26 03:19:11 +0300

    Check if fake.yaml exists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 
 import java.nio.file.Path
+import kotlin.io.path.notExists
 import kotlin.system.exitProcess
 
 
 fun main(args: Array<String>) {
-
     if (args.isEmpty()) {
         println("Not enough arguments!")
         exitProcess(1)
     }
 
-    val res = Task.fromFile(Path.of("fake.yaml"))
+    val path = Path.of("fake.yaml")
+    if (path.notExists()) {
+        println("There's no fake.yaml in this directory")
+        exitProcess(1)
+    }
+
+    val res = Task.fromFile(path)
 
     if (res != null) {
         val eqb = ExecutionQueueBuilder(res)
